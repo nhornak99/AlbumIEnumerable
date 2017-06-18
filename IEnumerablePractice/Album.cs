b5129683968247170d7113a3e61265c4b937ccee
Noah Hornak <nhornak99@gmail.com>
2017-06-18 00:07:37 -0400

    init

---
+++ IEnumerablePractice/Album.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IEnumerablePractice
+{
+    class Album : IEnumerable
+    {
+        List<Album> albums = null;
+        Album[] albumArray = null;
+        int index = 0;
+
+        public string Artist { get; set; }
+        public string Name { get; set; }
+
+        public Album()
+        {
+            albums = new List<Album>();
+            albumArray = new Album[10];
+        }
+
+        public void AddToList(Album album)
+        {
+            albums.Add(album);
+        }
+
+        public void AddToArray(Album album)
+        {
+            albumArray[index] = album;
+            index++;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Console.WriteLine("Album List:");
+
+            foreach (Album a in albums)
+            {
+                if (a == null)
+                {
+                    break;
+                }
+                yield return a;
+            }
+
+            Console.WriteLine("\n" + "Album Array:");
+
+            foreach(Album a in albumArray)
+            {
+                if (a == null)
+                {
+                    break;
+                }
+                yield return a;
+            }
+        }
+    }
+}
---
+++ IEnumerablePractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerablePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Album MOTM = new Album();
+            MOTM.Artist = "Kid Cudi";
+            MOTM.Name = "Man on The Moon: The End of Day";
+
+            Album TLOP = new Album();
+            TLOP.Artist = "Kanye West";
+            TLOP.Name = "The Life of Pablo";
+
+            Album albumList = new Album();
+            albumList.AddToList(MOTM);
+            albumList.AddToList(TLOP);
+            albumList.AddToArray(MOTM);
+            albumList.AddToArray(TLOP);
+
+            foreach(Album a in albumList)
+            {
+                Console.WriteLine(a.Name + " by " + a.Artist);
+            }
+            Console.ReadLine();
+        }
+    }
+}
